b83b0fca635f03d5f8467d90bbf8dd93ca9d62a7
Carlos Henrique Lima Melara <charlesmelara@outlook.com>
2019-06-18 17:15:53 -0300

    Método de Borwein com long double

---
+++ Borwein.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main () {
+	//inicialização de valores
+	int n = 1000000000, i = 0;
+	long double an, bn, pn, an1, bn1, pn1;
+
+	an = sqrtl(2.0000000000);
+	bn = 0.0000000000;
+	pn = 2.000000 + an;
+
+	while (i < n) {
+		an1 =  (sqrtl(an) + 1.000000 / sqrtl(an)) / 2.000000 ;
+		bn1 = ((1.00000000000000 + bn) * sqrtl(an)) / (an + bn);
+		pn1 = ((1.0000000 + an1) * pn * bn1) / (1.0000000 + bn1);
+
+		an = an1;
+		bn = bn1;
+		pn = pn1;
+
+		i++;
+		printf("pi it. %d: %Lf\n", i, pn1);
+	}
+	//printf("an: %Lf\nbn:%Lf\ntn:%Lf\npn:%Lf\n", an, bn, tn, pn);
+	return 0;
+	
+}
